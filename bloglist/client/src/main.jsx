a6e450408840b9162c50c901414390577bff978a
a-terho <140437240+a-terho@users.noreply.github.com>
2026-05-14 06:27:20 +0000

    Merge frontend and backend

--- bloglist/client/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>,
-);
